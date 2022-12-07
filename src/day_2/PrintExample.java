f8f8757c12eeb5a1c90543a1967382450ede2fa8
kimminseung <kimrlaks@gmail.com>
2022-12-07 13:42:00 +0900

    day_02

---
+++ src/day_2/PrintExample.java
@@ -0,0 +1,40 @@
+package day_2;
+
+public class PrintExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("");
+		System.out.printf("테스트");
+		System.out.printf("테스트");
+		System.out.printf("테스트");
+		System.out.println("");
+		System.out.print("테스트");
+		System.out.print("테스트");
+		System.out.print("테스트");
+		System.out.println("");
+		
+		int iNum1 = 10;
+		int iNum2 = 5;
+		int temp;
+		
+		temp = iNum1 + iNum2;
+		
+		System.out.println(temp);
+		
+		int x = 10;
+		int y = 5;
+		int sum = x + x * y/2;
+		
+		System.out.println(sum);
+		
+		System.out.printf("%f\n",2.5);
+		System.out.printf("%.1f\n",2.5);
+		
+		System.out.println("\u0041");
+		
+	}
+
+}
---
+++ src/day_2/ScannerExample1.java
@@ -0,0 +1,25 @@
+package day_2;
+import java.util.Scanner; 
+
+public class ScannerExample1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		/*System.out.print("값을 입력하시오:");
+		int input1 = sc.nextInt();
+		System.out.println(input1);
+		
+		System.out.print("실수만 입력해 :");
+		double input2 = sc.nextDouble();
+		System.out.println(input2);
+		*/
+		
+		System.out.print("입력1:");
+		String input3 = sc.nextLine();
+		System.out.println(input3);
+		
+
+	}
+
+}
